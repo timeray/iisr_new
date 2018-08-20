92ca59cecc154d9d9f5af041212152210f4a6401
artem <artemsetov@gmail.com>
2018-08-20 13:03:51 +0900

    Now Units are json-serializable and can be compared for equality

--- iisr/units.py
+++ iisr/units.py
@@ -1,16 +1,24 @@
 """
 Classes for physical units.
 """
+import json
 from typing import Union
 import numpy as np
 
 
+JSON_UNIT_TYPE_STR = '_unit_type'
+JSON_UNIT_VALUE_STR = 'value'
+
+_unit_types_registry = {}
+
+
+def register_unit(cls):
+    _unit_types_registry[cls.__name__] = cls
+    return cls
+
+
 class Unit:
     available_units = {}
-
-    @property
-    def value(self):
-        return self._value
 
     def __init__(self, value, unit):
         if unit not in self.available_units:
@@ -45,19 +53,66 @@ class Unit:
         return self._value
 
     def __str__(self):
-        return str(self._value)
+        if self.size == 1 and isinstance(self._value, np.ndarray):
+            value = self._value.item()
+        else:
+            value = self._value
+        return str(value) + ' ' + str(self._cur_unit)
 
     def __repr__(self):
-        return repr(self._value)
+        return '{}({}, {})'.format(self.__class__.__name__, repr(self._value), repr(self._cur_unit))
+
+    def __eq__(self, other: 'Unit'):
+        if self.__class__.__name__ != other.__class__.__name__:
+            raise TypeError('Wrong unit {} (expected {})'.format(
+                repr(other), self.__class__.__name__)
+            )
+        ratio = self.available_units[self._cur_unit] / other.available_units[other._cur_unit]
+        if self._value * ratio == other._value:
+            return True
+        else:
+            return False
 
 
+@register_unit
 class Frequency(Unit):
     available_units = {'Hz': 1., 'kHz': 1e3, 'MHz': 1e6}
 
 
+@register_unit
 class TimeUnit(Unit):
     available_units = {'s': 1., 'ms': 1e-3, 'us': 1e-6}
 
 
+@register_unit
 class Distance(Unit):
     available_units = {'mm': 1e-3, 'm': 1., 'km': 1e3}
+
+
+def get_unit_by_name(name):
+    return _unit_types_registry[name]
+
+
+class UnitsJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Unit):
+            if obj.size != 1:
+                raise TypeError('Only 1-sized Units may be json-serialized')
+            return {JSON_UNIT_TYPE_STR: obj.__class__.__name__, JSON_UNIT_VALUE_STR: obj.__str__()}
+
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+
+class UnitJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, obj):
+        if JSON_UNIT_TYPE_STR not in obj:
+            return obj
+        type = get_unit_by_name(obj[JSON_UNIT_TYPE_STR])
+        value_str = obj[JSON_UNIT_VALUE_STR].split()
+        value = float(value_str[0])
+        unit = (value_str[1])
+        return type(value, unit)
--- tests/test_units.py
+++ tests/test_units.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 from iisr import units
 import numpy as np
+import json
 
 
 def setup():
@@ -40,6 +41,57 @@ class TestUnits(TestCase):
         np.testing.assert_almost_equal(freq_unit['kHz'], test_freq)
         np.testing.assert_almost_equal(freq_unit['MHz'], test_freq / 1000.)
         self.assertEqual(freq_unit.size, size)
+
+    def test_comparison(self):
+        freq1 = units.Frequency(1000, 'kHz')
+        freq2 = units.Frequency(1000, 'kHz')
+        self.assertEqual(freq1, freq2)
+
+        freq2 = units.Frequency(1, 'MHz')
+        self.assertEqual(freq1, freq2)
+
+        freq2 = units.Frequency(2000, 'kHz')
+        self.assertNotEqual(freq1, freq2)
+
+        t = units.TimeUnit(1, 's')
+
+        with self.assertRaises(TypeError):
+            freq1 == t
+
+    def test_registry(self):
+        self.assertIn('Frequency', units._unit_types_registry)
+        self.assertIs(units._unit_types_registry['Frequency'], units.Frequency)
+        self.assertIn('TimeUnit', units._unit_types_registry)
+        self.assertIn('Distance', units._unit_types_registry)
+
+    def test_json_serialization(self):
+        test_freq = units.Frequency(1.5, 'kHz')
+        freq = json.loads(
+            json.dumps(test_freq, cls=units.UnitsJSONEncoder),
+            cls=units.UnitJSONDecoder,
+        )
+        self.assertEqual(freq, test_freq)
+
+        # Intended version should work too
+        test_dist = units.Distance(1.5, 'km')
+        dist = json.loads(
+            json.dumps(test_dist, indent=True, cls=units.UnitsJSONEncoder),
+            cls=units.UnitJSONDecoder,
+        )
+        self.assertEqual(dist, test_dist)
+
+        # Array serialization (with size > 1) is not allowed
+        arr_time = units.TimeUnit(np.array([1., 2., 3.]), 's')
+        with self.assertRaises(TypeError):
+            json.dumps(arr_time, cls=units.UnitsJSONEncoder)
+
+        # Numpy arrays with size = 1 will be serialized and decoded later as floats
+        arr_size1_time = units.TimeUnit(np.array([1.]), 's')
+        decoded_time = json.loads(
+            json.dumps(arr_size1_time, cls=units.UnitsJSONEncoder),
+            cls=units.UnitJSONDecoder
+        )
+        self.assertEqual(arr_size1_time, decoded_time)
 
 
 if __name__ == '__main__':
