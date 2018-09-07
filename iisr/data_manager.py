1195f243837ad84fd7373a5919bbf94bca1628ce
artem <artemsetov@gmail.com>
2018-09-07 13:28:49 +0900

    Change naming in data manager and add tests

--- iisr/data_manager.py
+++ iisr/data_manager.py
@@ -1,123 +1,70 @@
 """
 Manages output files and results of processing.
 """
-from iisr.representation import FirstStageResults, SecondStageResults
+from iisr.preprocessing.representation import HandlerResult
 import os
 import sys
 from pathlib import Path
 import configparser
-from datetime import date
 from iisr.utils import DATE_FMT
 
 
-DEFAULT_CONFIG = Path('..') / 'general_config.ini'
+DEFAULT_CONFIG = Path(__file__).parent.parent / 'general_config.ini'
+config = configparser.ConfigParser()
+config.read(str(DEFAULT_CONFIG))
+CONFIG_MAIN_FOLDER = Path(config['Common']['path_to_output_storage'])
+if not CONFIG_MAIN_FOLDER.exists():
+    CONFIG_MAIN_FOLDER.mkdir()
+CONFIG_MAIN_FOLDER = CONFIG_MAIN_FOLDER.resolve()
 
 
 class DataManager:
     """
     Data manager controls output files of processing.
     """
-    FIRST_STAGE_FOLDER_NAME = 'first_stage'
-    SECOND_STAGE_FOLDER_NAME = 'second_stage'
-    ID_REGISTRY = '.id'
+    PREPROCESSING_FOLDER_NAME = 'pre_proc'
+    POSTPROCESSING_FOLDER_NAME = 'post_proc'
 
-    def __init__(self, path_to_config=DEFAULT_CONFIG):
+    def __init__(self, main_folder_path: Path = CONFIG_MAIN_FOLDER):
         """
-        Parameters
-        ----------
-        path_to_config: str
-            Path to program basic configuration file.
+
+        Args:
+            main_folder_path:
         """
-        config = configparser.ConfigParser()
-        config.read(path_to_config)
-        self.main_folder = Path(config['Common']['path_to_output_storage']).resolve()
+        self.main_folder = main_folder_path
 
         if not self.main_folder.exists():
             self.main_folder.mkdir()
         elif not self.main_folder.is_dir():
             raise NotADirectoryError('{}'.format(self.main_folder))
 
-        self.first_stage_folder = self.main_folder / self.FIRST_STAGE_FOLDER_NAME
-        self.second_stage_folder = self.main_folder / self.SECOND_STAGE_FOLDER_NAME
-        self.id_registry_path = self.main_folder / self.ID_REGISTRY
-
-        if not os.path.exists(self.first_stage_folder):
-            self.first_stage_folder.mkdir()
-
-        if not os.path.exists(self.second_stage_folder):
-            self.second_stage_folder.mkdir()
-
-        # if self.id_registry_path.exists():
-        #     with open(self.id_registry_path) as id_file:
-        #         self.current_id = int(id_file.readlines()[-1].split(' ')[-1])
-        # else:
-        #     self.current_id = ID(0)
-        #     self._write_new_id(self.current_id)
-
-    def _write_new_id(self, new_id):
-        """
-        Write new id to ID_REGISTRY.
-
-        Parameters
-        ----------
-        new_id: ID
-        """
-        if not isinstance(new_id, ID):
-            raise ValueError('Incorrect ID: {}'.format(new_id))
-
-        with open(str(self.id_registry_path), 'w') as id_file:
-            id_file.write('# Store global identifier. Do not delete or change.\n')
-            id_file.write('CURRENT_ID = {}'.format(new_id))
-
-    def get_path(self, save_date: date = None) -> Path:
-        path = self.first_stage_folder
-        if save_date is not None:
-            path = path / save_date.strftime(DATE_FMT)
-        return path
-
-    def get_new_id(self):
-        """Return new unique ID"""
-        self.current_id += 1
-        self._write_new_id(self.current_id)
-        return ID(self.current_id)
-
-    def get_results(self, data_id):
-        """
-        Return first stage results given ID.
-
-        Parameters
-        ----------
-        data_id: int or ID
-            Unique first stage data ID.
-
-        Returns
-        -------
-        results: Results
-        """
-
-    def save_first_stage_results(self, results: FirstStageResults):
+    def save_preprocessing_result(self, result: HandlerResult):
         """
         Save first stage processing results.
 
         Parameters
         ----------
-        results: FirstStageResults
+        result: FirstStageResults
 
         Returns
         -------
         id: ID
             Unique identifier of results.
         """
-        for result in results.results:
-            params_str = result.parameters_short_name()
+        name = result.short_name
 
-            for result_date in result.dates:
-                date_path = self.get_path(save_date=result_date)
-                mode_directory = result.mode_name
-                path = date_path / mode_directory
-                result.save_txt(path, save_date=result_date)
+        for result_date in result.dates:
+            date_str = result_date.strftime(DATE_FMT)
+            filename = result.mode_name + '_' + name + '.dat'
+            dirpath = self.main_folder / date_str / self.PREPROCESSING_FOLDER_NAME
 
-    def save_second_stage_results(self, results):
+            if not dirpath.exists():
+                dirpath.mkdir(parents=True)
+
+            with open(str(dirpath / filename), 'w') as file:
+                result.save_txt(file, save_date=result_date)
+
+    def save_postprocessing_results(self, results):
         """
         Save second stage processing results.
 
@@ -157,21 +104,3 @@ class DataManager:
             msg.append('> '.join(line))
             nesting.append(dirpath)
         print('\n'.join(msg), file=file)
-
-    def print_id(self, results_id, file=sys.stdout):
-        """
-        Print results for given id.
-
-        Parameters
-        ----------
-        results_id: int
-        file: stream
-        """
-        results = self.get_results(results_id)
-        print(results, file=file)
-
-
-class ID(int):
-    """Store ID"""
-
-
---
+++ iisr/tests/test_data_manager.py
@@ -0,0 +1,42 @@
+from unittest import TestCase, main, mock
+from iisr.data_manager import *
+from iisr.preprocessing.representation import HandlerResult
+import tempfile
+from pathlib import Path
+from datetime import datetime
+from typing import IO, List
+
+
+class TestDataManager(TestCase):
+    def test_init(self):
+        with tempfile.TemporaryDirectory() as temp_dir_name:
+            manager = DataManager(Path(temp_dir_name))
+            self.assertTrue(hasattr(manager, 'main_folder'))
+            self.assertTrue(manager.main_folder.exists())
+            self.assertTrue(manager.main_folder.is_dir())
+
+    def test_save_preproc(self):
+        result = mock.Mock()
+        dtime = datetime(2015, 1, 1)
+        mode_name = 'test'
+        short_name = 'short_name'
+        preproc_dir = DataManager.PREPROCESSING_FOLDER_NAME
+        type(result).dates = mock.PropertyMock(return_value=[dtime])
+        type(result).mode_name = mock.PropertyMock(return_value=mode_name)
+        type(result).short_name = mock.PropertyMock(return_value=short_name)
+        result.save_txt.return_value = None
+
+        with tempfile.TemporaryDirectory() as temp_dir_name:
+            dirpath = Path(temp_dir_name)
+            manager = DataManager(dirpath)
+            manager.save_preprocessing_result(result)
+
+            test_filename = mode_name + '_' + short_name + '.dat'
+            test_path = str(dirpath / dtime.strftime(DATE_FMT) / preproc_dir / test_filename)
+
+            self.assertTrue(result.save_txt.called)
+            self.assertEqual(result.save_txt.call_args[0][0].name, test_path)
+
+
+if __name__ == '__main__':
+    main()
